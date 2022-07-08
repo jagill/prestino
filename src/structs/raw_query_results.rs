9ffd4f3b0c62ce7b7eb58b1ed55f2cf2d1df2378
eltonpinto <eltonpinto@fb.com>
2022-07-08 10:08:44 -0700

    Implement serde for Option<Uri>
    Lifted solution out of https://github.com/serde-rs/serde/issues/1301

--- src/structs/raw_query_results.rs
+++ src/structs/raw_query_results.rs
@@ -1,4 +1,5 @@
 use super::{Column, QueryError};
+use crate::structs::misc::opt_uri_serde;
 use http::uri::Uri;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -10,7 +11,7 @@ pub struct RawQueryResults {
     pub id: String,
     #[serde(with = "http_serde::uri")]
     pub info_uri: Uri,
-    #[serde(with = "http_serde::uri")]
+    #[serde(default, with = "opt_uri_serde")]
     pub next_uri: Option<Uri>,
     pub columns: Vec<Column>,
     pub data: Vec<Value>,
