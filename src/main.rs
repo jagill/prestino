ddd98cc07c597800b69da1992132073b62059b94
James Gill <jamesagill@gmail.com>
2022-11-13 10:08:04 -0500

    Streaming results from statement_executor
    StatementExecutor now has methods to return streaming results.
    It has query_results, which gives every result; batches, which
    gives only non-empty results; and rows, which gives results row-by-row.
    
    These are early prototypes and subject to change.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
+use futures::StreamExt;
 use prestino::PrestoClient;
-use tokio::io::{stdout, AsyncWriteExt as _};
+use tokio::io::AsyncWriteExt as _;
+use futures_util::pin_mut;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let client = PrestoClient::new("http://localhost:8080".to_owned());
-    let mut executor = client.execute("SELECT 1 AS a".to_owned()).await?;
-    executor.run().await?;
+
+    println!("Iterate results");
+    let executor = client.execute("SELECT 1 AS a".to_owned()).await?;
+    let query_results = executor.query_results();
+    pin_mut!(query_results);
+    while let Some(result) = query_results.next().await {
+        println!("Rows {:?}", result);
+    }
+
+    println!("Iterate batches");
+    let executor = client.execute("SELECT 1 AS a".to_owned()).await?;
+    let batches = executor.batches();
+    pin_mut!(batches);
+    while let Some(batch) = batches.next().await {
+        println!("Batch {:?}", batch);
+    }
+
+    println!("Iterate rows");
+    let executor = client.execute("SELECT 1 AS a".to_owned()).await?;
+    let rows = executor.rows();
+    pin_mut!(rows);
+    while let Some(row) = rows.next().await {
+        println!("Row {:?}", row);
+    }
     Ok(())
 }
--- src/statement_executor.rs
+++ src/statement_executor.rs
@@ -1,8 +1,12 @@
 use crate::error::Error;
 use crate::results::QueryResultsValue;
 use crate::PrestoApi;
+use async_stream::try_stream;
+use futures::Stream;
+use futures_util::pin_mut;
 use hyper::client::HttpConnector;
 use hyper::{Client, Uri};
+use serde_json::Value;
 
 pub struct StatementExecutor {
     id: String,
@@ -23,30 +27,53 @@ impl StatementExecutor {
         &self.id
     }
 
-    pub async fn run(&mut self) -> Result<(), Error> {
-        if let Some(data) = self.results.data.take() {
-            println!("{:?}", data);
-        }
-
-        let mut uri_opt = self.results.next_uri.take();
-        while let Some(uri) = uri_opt {
-            println!("Getting next uri {uri}");
-            self.fetch_next_results(uri).await?;
-            uri_opt = self.results.next_uri.take();
-            if let Some(data) = self.results.data.take() {
-                println!("{:?}", data);
-            }
-        }
-
+    // Fetch and store (not return) next results given a uri
+    async fn _fetch_next_results(&mut self, uri: Uri) -> Result<(), Error> {
+        let request = PrestoApi::get_results_request(uri)?;
+        let results = PrestoApi::get_results(request, &self.http_client).await?;
+        self.results = results;
         Ok(())
     }
 
-    // Fetch and store (not return) next results
-    pub async fn fetch_next_results(&mut self, uri: Uri) -> Result<(), Error> {
-        let request = PrestoApi::get_results_request(uri)?;
-        let results = PrestoApi::get_results(request, &self.http_client).await?;
-        println!("{:?}", results.stats);
-        self.results = results;
-        Ok(())
+    /// Fetch and store (not return) next QueryResultsValue
+    pub async fn fetch_next_results(&mut self) -> Option<Result<(), Error>> {
+        let next_uri = self.results.next_uri.take()?;
+        Some(self._fetch_next_results(next_uri).await)
+    }
+
+    pub fn query_results(mut self) -> impl Stream<Item = Result<Vec<Value>, Error>> {
+        try_stream! {
+            yield self.results.data.take().unwrap_or_default();
+
+            while let Some(next_uri) = self.results.next_uri.take() {
+                self._fetch_next_results(next_uri).await?;
+                yield self.results.data.take().unwrap_or_default();
+            }
+        }
+    }
+
+    pub fn batches(self) -> impl Stream<Item = Result<Vec<Value>, Error>> {
+        try_stream! {
+            let query_results = self.query_results();
+            pin_mut!(query_results);
+            for await rows_result in query_results {
+                let rows = rows_result.unwrap();
+                if !rows.is_empty() {
+                    yield rows;
+                }
+            }
+        }
+    }
+
+    pub fn rows(self) -> impl Stream<Item = Result<Value, Error>> {
+        try_stream! {
+            let batches = self.batches();
+            pin_mut!(batches);
+            for await batch in batches {
+                for row in batch?.drain(..) {
+                    yield row;
+                }
+            }
+        }
     }
 }
