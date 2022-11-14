864ccac172819030efb0262fc98bf913384fbdec
James Gill <jamesagill@gmail.com>
2022-11-14 10:52:26 -0500

    WIP initial work to make a CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::marker::PhantomData;
+
 use futures::StreamExt;
-use prestino::PrestoClient;
-use tokio::io::AsyncWriteExt as _;
 use futures_util::pin_mut;
+use prestino::{results::Column, PrestoClient};
+use serde_json::Value;
+use tokio::io::AsyncWriteExt as _;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -30,5 +33,62 @@ async fn main() -> Result<(), anyhow::Error> {
     while let Some(row) = rows.next().await {
         println!("Row {:?}", row);
     }
+    Ok(())
+}
+
+enum OutputFormat {
+    JSONL,
+    XSV { header: bool, delimiter: String },
+}
+
+enum OutputMode {
+    Response,
+    Batch,
+    Row,
+}
+
+struct Outputter {
+    output_format: OutputFormat,
+    columns: Vec<Column>,
+}
+
+impl Outputter {
+    pub fn maybe_output_header(&self) {
+        if let OutputFormat::XSV {
+            header: true,
+            delimiter,
+        } = self.output_format
+        {
+            println!(
+                "{}",
+                self.columns
+                    .iter()
+                    .map(|col| col.name())
+                    .collect::<Vec<&str>>()
+                    .join(&delimiter)
+            );
+        }
+    }
+    pub fn output(&self, val: Value) {}
+}
+
+async fn run(
+    host: &str,
+    query: &str,
+    output_format: OutputFormat,
+    output_mode: OutputMode,
+) -> Result<(), anyhow::Error> {
+    let client = PrestoClient::new(host.to_owned());
+    let executor = client.execute(query.to_owned()).await?;
+    let stream = match output_mode {
+        OutputMode::Response => executor.query_results(),
+        OutputMode::Batch => executor.batches(),
+        OutputMode::Row => executor.rows(),
+    };
+    pin_mut!(stream);
+    while let Some(item) = stream.next().await {
+        println!("Rows {:?}", result);
+    }
+
     Ok(())
 }
