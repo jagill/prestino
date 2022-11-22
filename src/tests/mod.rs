6ba97cb386c9f6f5518417eb8a573482ee863c54
James Gill <jamesagill@gmail.com>
2022-11-22 09:15:50 -0500

    A tool to mock out REST responses for given data
    We want to be able to mock out responses for data, so that
    we can test many cases (nested data structures, errors, etc).
    
    This uses wiremock to mock out the REST server, and tools for
    us to queue up responses to be served.  That way we can test
    how we handle such responses.
    
    This is sort of awkwardly placed; the dirtree should be improved.

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -10,12 +10,12 @@ mod tests {
     use serde_json::{json, Value};
     use wiremock::MockServer;
 
-    #[tokio::test]
-    async fn test_basic_flow_1() {
+
+    async fn get_rows(response_strs: &[&str]) -> Result<Vec<Value>, Error> {
         let mock_server = MockServer::start().await;
         let base_uri = mock_server.uri();
         println!("{base_uri}");
-        let responses = ResponseChain::new(super::response_set_1::RESPONSES, base_uri);
+        let responses = ResponseChain::new(response_strs, base_uri);
         responses.mock_flow(&mock_server).await;
 
         let presto_client = PrestoClient::new(mock_server.uri());
@@ -24,6 +24,45 @@ mod tests {
         let stream = executor.rows();
         pin_mut!(stream);
         let rows: Result<Vec<Value>, Error> = stream.try_collect().await;
+        rows
+    }
+
+    #[tokio::test]
+    async fn test_basic_flow_1() {
+        let rows = get_rows(super::response_set_1::RESPONSES).await;
         assert_eq!(rows.unwrap(), vec![json!([1])],);
+    }
+
+    #[tokio::test]
+    async fn test_basic_types() {
+        let response_strs: Vec<String> = ResponseChain::make_response_set(
+            &[
+                ("a_bool", "boolean"),
+                ("a_int8", "tinyint"),
+                ("a_int16", "smallint"),
+                ("a_int32", "integer"),
+                ("a_int64", "bigint"),
+                ("a_float32", "real"),
+                ("a_float64", "double"),
+                ("a_str", "varchar"),
+            ],
+            &[
+                json!([
+                    [true, 1, 2, 3, 4, 1.1, 2.2, "a"],
+                    [false, -1, -2, -3, -4, -1.1, -2.2, "b"]
+                ]),
+                json!([[null, null, null, null, null, null, null, null]]),
+            ],
+        );
+        let response_ref: Vec<&str> = response_strs.iter().map(AsRef::as_ref).collect();
+        let rows = get_rows(&response_ref).await;
+        assert_eq!(
+            rows.unwrap(),
+            vec![
+                json!([true, 1, 2, 3, 4, 1.1, 2.2, "a"]),
+                json!([false, -1, -2, -3, -4, -1.1, -2.2, "b"]),
+                json!([null, null, null, null, null, null, null, null]),
+            ],
+        );
     }
 }
--- src/tests/response_chain.rs
+++ src/tests/response_chain.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, MockServer, ResponseTemplate};
 
@@ -37,12 +38,7 @@ impl ResponseChain {
     fn extract_next_uri(response_str: &str, base_uri: &str) -> Option<String> {
         let NextUri { next_uri } = serde_json::from_str(response_str).unwrap();
 
-        Some(
-            next_uri?
-                .strip_prefix(base_uri)
-                .unwrap()
-                .to_string(),
-        )
+        Some(next_uri?.strip_prefix(base_uri).unwrap().to_string())
     }
 
     pub async fn mock_flow(&self, mock_server: &MockServer) {
@@ -63,5 +59,31 @@ impl ResponseChain {
                 .mount(&mock_server)
                 .await;
         }
+    }
+
+    pub fn make_response_set(columns: &[(&str, &str)], data: &[Value]) -> Vec<String> {
+        let id = uuid::Uuid::new_v4();
+        let column_str = columns.iter()
+            .map(|(name, typ)| format!(r#"{{"name":"{name}","type":"{typ}","typeSignature":{{"rawType":"{typ}","arguments":[]}}}}"#))
+            .collect::<Vec<String>>().join(",");
+        println!("Column str: {column_str}");
+
+        let mut responses = Vec::new();
+        // Queued
+        responses.push(format!(r#"{{"id":"{id}","infoUri":"http://localhost:8080/ui/query.html?{id}","nextUri":"http://localhost:8080/v1/statement/queued/{id}/ye6b7b80fe5070e687a6f31e87c69b2157bc560b6/1","stats":{{"state":"QUEUED","queued":true,"scheduled":false,"nodes":0,"totalSplits":0,"queuedSplits":0,"runningSplits":0,"completedSplits":0,"cpuTimeMillis":0,"wallTimeMillis":0,"queuedTimeMillis":0,"elapsedTimeMillis":0,"processedRows":0,"processedBytes":0,"physicalInputBytes":0,"peakMemoryBytes":0,"spilledBytes":0}},"warnings":[]}}"#));
+        // Running, columns but no data
+        responses.push(format!(r#"{{"id":"{id}","infoUri":"http://localhost:8080/ui/query.html?{id}","partialCancelUri":"http://localhost:8080/v1/statement/executing/partialCancel/{id}/0/y6aab6ff9dc9b744da35e343b3ed6b6b3f5a1bc4d/1","nextUri":"http://localhost:8080/v1/statement/executing/{id}/y6aab6ff9dc9b744da35e343b3ed6b6b3f5a1bc4d/1","columns":[{column_str}],"stats":{{"state":"RUNNING","queued":false,"scheduled":true,"nodes":1,"totalSplits":0,"queuedSplits":0,"runningSplits":0,"completedSplits":0,"cpuTimeMillis":0,"wallTimeMillis":0,"queuedTimeMillis":3,"elapsedTimeMillis":60,"processedRows":0,"processedBytes":0,"physicalInputBytes":0,"peakMemoryBytes":0,"spilledBytes":0,"rootStage":{{"stageId":"0","state":"RUNNING","done":false,"nodes":1,"totalSplits":0,"queuedSplits":0,"runningSplits":0,"completedSplits":0,"cpuTimeMillis":0,"wallTimeMillis":0,"processedRows":0,"processedBytes":0,"physicalInputBytes":0,"failedTasks":0,"coordinatorOnly":false,"subStages":[]}}}},"warnings":[]}}"#));
+        for (idx, rows) in data.iter().enumerate() {
+            // Data row; can have empty ("[]") data.
+            let data_str = serde_json::to_string(rows).unwrap();
+            println!("Data str: {data_str}");
+            responses.push(format!(r#"{{"id":"{id}","infoUri":"http://localhost:8080/ui/query.html?{id}","partialCancelUri":"http://localhost:8080/v1/statement/executing/partialCancel/{id}/0/y0f368c8309d5e2d7dd10875b57cb880e862ab0cc/2","nextUri":"http://localhost:8080/v1/statement/executing/{id}/y0f368c8309d5e2d7dd10875b57cb880e862ab0cc/{idx}","columns":[{column_str}],"data":{data_str},"stats":{{"state":"RUNNING","queued":false,"scheduled":true,"nodes":1,"totalSplits":1,"queuedSplits":1,"runningSplits":0,"completedSplits":0,"cpuTimeMillis":0,"wallTimeMillis":0,"queuedTimeMillis":3,"elapsedTimeMillis":85,"processedRows":0,"processedBytes":0,"physicalInputBytes":0,"peakMemoryBytes":103,"spilledBytes":0,"rootStage":{{"stageId":"0","state":"RUNNING","done":false,"nodes":1,"totalSplits":1,"queuedSplits":1,"runningSplits":0,"completedSplits":0,"cpuTimeMillis":0,"wallTimeMillis":0,"processedRows":0,"processedBytes":0,"physicalInputBytes":0,"failedTasks":0,"coordinatorOnly":false,"subStages":[]}},"progressPercentage":0.0}},"warnings":[]}}"#));
+        }
+        // Finished, next_uri (stats still calculating)
+        responses.push(format!(r#"{{"id":"{id}","infoUri":"http://localhost:8080/ui/query.html?{id}","nextUri":"http://localhost:8080/v1/statement/executing/{id}/y0566e949b2111366df34de9d56ebb7cbd3e9a45c/3","columns":[{column_str}],"stats":{{"state":"FINISHED","queued":false,"scheduled":true,"nodes":1,"totalSplits":1,"queuedSplits":1,"runningSplits":0,"completedSplits":0,"cpuTimeMillis":0,"wallTimeMillis":0,"queuedTimeMillis":3,"elapsedTimeMillis":89,"processedRows":0,"processedBytes":0,"physicalInputBytes":0,"peakMemoryBytes":103,"spilledBytes":0,"rootStage":{{"stageId":"0","state":"FINISHED","done":true,"nodes":1,"totalSplits":1,"queuedSplits":1,"runningSplits":0,"completedSplits":0,"cpuTimeMillis":0,"wallTimeMillis":0,"processedRows":0,"processedBytes":0,"physicalInputBytes":0,"failedTasks":0,"coordinatorOnly":false,"subStages":[]}},"progressPercentage":0.0}},"warnings":[]}}"#));
+        // Finished, no next_uri
+        responses.push(format!(r#"{{"id":"{id}","infoUri":"http://localhost:8080/ui/query.html?{id}","columns":[{column_str}],"stats":{{"state":"FINISHED","queued":false,"scheduled":true,"nodes":1,"totalSplits":1,"queuedSplits":0,"runningSplits":0,"completedSplits":1,"cpuTimeMillis":1,"wallTimeMillis":1,"queuedTimeMillis":3,"elapsedTimeMillis":89,"processedRows":0,"processedBytes":0,"physicalInputBytes":0,"peakMemoryBytes":103,"spilledBytes":0,"rootStage":{{"stageId":"0","state":"FINISHED","done":true,"nodes":1,"totalSplits":1,"queuedSplits":0,"runningSplits":0,"completedSplits":1,"cpuTimeMillis":1,"wallTimeMillis":1,"processedRows":1,"processedBytes":0,"physicalInputBytes":0,"failedTasks":0,"coordinatorOnly":false,"subStages":[]}},"progressPercentage":100.0}},"warnings":[]}}"#));
+
+        responses
     }
 }
