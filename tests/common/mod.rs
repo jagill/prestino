21de40c0f963ce3a1b72bbff758958458c4ca4e5
James Gill <jamesagill@gmail.com>
2022-12-13 22:33:04 -0500

    Use execute_collect in tests, simplifying helper fn

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,13 +1,9 @@
-use futures::TryStreamExt;
-use futures_util::pin_mut;
 use prestino::{PrestinoClient, PrestinoError};
 use serde::de::DeserializeOwned;
 
 pub async fn get_rows<T: DeserializeOwned>(sql: &str) -> Result<Vec<T>, PrestinoError> {
-    let client = PrestinoClient::trino("http://localhost:8080".to_owned()).user("me");
-    let executor = client.execute(sql.to_string()).await?;
-    let stream = executor.rows();
-    pin_mut!(stream);
-    let rows: Result<Vec<T>, PrestinoError> = stream.try_collect().await;
-    rows
+    PrestinoClient::trino("http://localhost:8080".to_owned())
+        .user("me")
+        .execute_collect(sql.to_string())
+        .await
 }
